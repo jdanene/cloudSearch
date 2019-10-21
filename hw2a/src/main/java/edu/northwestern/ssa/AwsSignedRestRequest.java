be0126d03e59de7af65505063bab87500fe72d0a
Jidé Anene <dominicanene@gmail.com>
2019-10-20 23:58:04 -0500

    works no testing

--- hw2a/src/main/java/edu/northwestern/ssa/AwsSignedRestRequest.java
+++ hw2a/src/main/java/edu/northwestern/ssa/AwsSignedRestRequest.java
@@ -1,7 +1,11 @@
 package edu.northwestern.ssa;
 
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.signer.Aws4Signer;
 import software.amazon.awssdk.auth.signer.params.Aws4SignerParams;
 import software.amazon.awssdk.http.*;
@@ -11,6 +15,8 @@ import software.amazon.awssdk.utils.StringInputStream;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,9 +25,14 @@ import java.util.Optional;
  * https://github.com/aws/aws-sdk-java-v2/issues/339
  */
 public class AwsSignedRestRequest implements Closeable {
+    final Logger LOG = LoggerFactory.getLogger(AwsSignedRestRequest.class);
+    Duration duration = Duration.ofMillis(120000);
     private Aws4SignerParams params;
     private Aws4Signer signer = Aws4Signer.create();
-    private SdkHttpClient httpClient = ApacheHttpClient.builder().build();
+    private SdkHttpClient httpClient = ApacheHttpClient.
+            builder().
+            connectionTimeout(duration)
+            .build();
 
     /** @param serviceName would be "es" for Elasticsearch */
     public AwsSignedRestRequest(String serviceName) {
@@ -32,16 +43,46 @@ public class AwsSignedRestRequest implements Closeable {
                 .signingName(serviceName)
                 .signingRegion(Region.US_EAST_2)
                 .build();
+
+        System.out.println(Config.getParam("AWS_ACCESS_KEY_ID"));
+        System.out.println(Config.getParam("AWS_SECRET_ACCESS_KEY"));
+
     }
 
     /** @param path should not have a leading "/" */
-    public HttpExecuteResponse restRequest(SdkHttpMethod method, String host, String path,
+    protected HttpExecuteResponse restRequest(SdkHttpMethod method, String host, String path,
                                               Optional<Map<String, String>> queryParameters)
             throws IOException {
         return restRequest(method, host, path, queryParameters, Optional.empty());
     }
 
-    public HttpExecuteResponse restRequest(SdkHttpMethod method, String host, String path,
+
+    /**
+     * Outputs the response body as a string given a HttpExecuteResponse
+     * @param httpExecuteResponse
+     * @return body of http response
+     */
+     static public String getResponseBody(HttpExecuteResponse httpExecuteResponse) throws IOException {
+        Optional<AbortableInputStream>  abortableInputStream = httpExecuteResponse.responseBody();
+        if (abortableInputStream.isPresent()){
+            return IOUtils.toString(abortableInputStream.get(), StandardCharsets.UTF_8.name());
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * Closes the httpExecuteResponse response so we wont get a wierd error.
+     * @param httpExecuteResponse
+     */
+    static void closeStream(HttpExecuteResponse httpExecuteResponse){
+        Optional<AbortableInputStream>  abortableInputStream = httpExecuteResponse.responseBody();
+        abortableInputStream.ifPresent(IOUtils::closeQuietly);
+    }
+
+
+
+    protected HttpExecuteResponse restRequest(SdkHttpMethod method, String host, String path,
                                               Optional<Map<String, String>> queryParameters,
                                               Optional<JSONObject> body)
             throws IOException {
@@ -69,6 +110,7 @@ public class AwsSignedRestRequest implements Closeable {
         // !!!: line below is necessary even though the contentStreamProvider is in the request.
         // Otherwise the body will be missing from the request and auth signature will fail.
         request.contentStreamProvider().ifPresent(c -> rb.contentStreamProvider(c));
+
         return httpClient.prepareRequest(rb.build()).call();
     }
 
--- hw2a/src/main/java/edu/northwestern/ssa/api/Search.java
+++ hw2a/src/main/java/edu/northwestern/ssa/api/Search.java
@@ -1,25 +1,178 @@
 package edu.northwestern.ssa.api;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiResponse;
+import com.wordnik.swagger.annotations.ApiResponses;
+import edu.northwestern.ssa.AwsSignedRestRequest;
+import edu.northwestern.ssa.database.ElasticSearch;
+import software.amazon.awssdk.http.HttpExecuteResponse;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.ws.rs.core.MediaType;
 
 @Path("/search")
 public class Search {
 
-    /** when testing, this is reachable at http://localhost:8080/api/search?query=hello */
-    @GET
-    public Response getMsg(@QueryParam("query") String q) throws IOException {
-        JSONArray results = new JSONArray();
-        results.put("hello world!");
-        results.put(q);
-        return Response.status(200).type("application/json").entity(results.toString(4))
-                // below header is for CORS
-                .header("Access-Control-Allow-Origin", "*").build();
+  ElasticSearch elasticSearch = new ElasticSearch();
+  static final String DATE_FORMAT = "YYYY-MM-DD";
+  Set<String> ISO_LANGUAGES = new HashSet<>(Arrays.asList(Locale.getISOLanguages()));
+
+  /**
+   * Checks a date is properly formatted.
+   *
+   * @param date string date
+   * @return boolean indicating if properly formatted date or not
+   * @implNote From https://stackoverflow.com/questions/226910/how-to-sanity-check-a-date-in-java
+   */
+  public static boolean isDateValid(String date) {
+    try {
+      DateFormat df = new SimpleDateFormat(DATE_FORMAT);
+      df.setLenient(false);
+      df.parse(date);
+      return true;
+    } catch (ParseException e) {
+      return false;
     }
+  }
+
+  public static String formatPayload(String payload) {
+    return null;
+  }
+
+  /**
+   * http://localhost:8080/api/search?query=Northwestern&count=01
+   * http://localhost:8080/api/search?query=Northwestern
+   * http://ssa-hw2-backend.stevetarzia.com/api/search?query=Northwestern
+   */
+  /**
+   * when testing, this is reachable at http://localhost:8080/api/search?query="hello bitch"%20hey
+   */
+  /** http://localhost:8080/api/search?query=hello%20bitch&language=espanol */
+  // http://ssa-hw2-backend.stevetarzia.com/api/search?query=Northwestern
+  @ApiOperation(value = "Searches AWS elastic search given query parameters")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Query Successful"),
+        @ApiResponse(code = 404, message = "Required query parameter missing"),
+        @ApiResponse(code = 500, message = "Internal Server Error"),
+        @ApiResponse(code = 502, message = "Bad gateway"),
+        @ApiResponse(code = 406, message = "Invalid value")
+      })
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getMsg(
+      @QueryParam("query") String queryParam,
+      @QueryParam("language") String languageParam,
+      @QueryParam("date") String dateParam,
+      @QueryParam("count") String countParam,
+      @QueryParam("offset") String offsetParam) {
+
+    // Check that required parameter "query" is present
+    if (queryParam == null) {
+      return Response.status(404)
+          .type(MediaType.TEXT_PLAIN)
+          .entity("Missing required parameter `query`")
+          .header("Access-Control-Allow-Origin", "*")
+          .build();
+    }
+
+    // Check that date is formatted as "YYYY-MM-DD"
+    Optional<String> optionalDateParam = Optional.ofNullable(dateParam);
+    if (optionalDateParam.isPresent()) {
+      if (!(isDateValid(optionalDateParam.get()))) {
+        return Response.status(406)
+            .type(MediaType.TEXT_PLAIN)
+            .entity("Invalid value: Date")
+            .header("Access-Control-Allow-Origin", "*")
+            .build();
+      }
+    }
+
+    // Check that language is of ISO
+    Optional<String> optionalLanguageParam = Optional.ofNullable(languageParam);
+    if (optionalLanguageParam.isPresent()) {
+      if (!(ISO_LANGUAGES.contains(optionalLanguageParam.get()))) {
+        return Response.status(406)
+            .type(MediaType.TEXT_PLAIN)
+            .entity("Invalid value: Language")
+            .header("Access-Control-Allow-Origin", "*")
+            .build();
+      }
+    }
+
+    // Check that count parameter is unsigned integer
+    Optional<String> optionalCountParam = Optional.ofNullable(countParam);
+    if (optionalCountParam.isPresent()) {
+      if (!(optionalCountParam.get().matches("^//d+$"))) {
+        return Response.status(406)
+            .type(MediaType.TEXT_PLAIN)
+            .entity("Invalid value: Count")
+            .header("Access-Control-Allow-Origin", "*")
+            .build();
+      }
+    }
+
+    // Check that offset parameter is unsigned integer
+    Optional<String> optionalOffsetParam = Optional.ofNullable(offsetParam);
+    if (optionalOffsetParam.isPresent()) {
+      if (!(optionalOffsetParam.get().matches("^//d+$"))) {
+        return Response.status(406)
+            .type(MediaType.TEXT_PLAIN)
+            .entity("Invalid value: Offset")
+            .header("Access-Control-Allow-Origin", "*")
+            .build();
+      }
+    }
+
+    // Post  query to ElasticSearch
+    ElasticSearch elasticSearch = new ElasticSearch();
+    HttpExecuteResponse httpExecuteResponse = null;
+    try {
+      httpExecuteResponse =
+          elasticSearch.queryElasticSearch(
+              queryParam,
+              optionalLanguageParam,
+              optionalDateParam,
+              optionalCountParam,
+              optionalOffsetParam);
+    } catch (IOException e) {
+      return Response.status(502)
+          .type(MediaType.TEXT_PLAIN)
+          .entity("Bad gateway")
+          .header("Access-Control-Allow-Origin", "*")
+          .build();
+    }
+
+    // Get the body of the response
+    String requestBody;
+    try {
+      requestBody =
+          ElasticSearch.formatPayload(AwsSignedRestRequest.getResponseBody(httpExecuteResponse));
+
+    } catch (IOException e) {
+      return Response.status(500)
+          .type(MediaType.TEXT_PLAIN)
+          .entity("Internal Server Error")
+          .header("Access-Control-Allow-Origin", "*")
+          .build();
+    }
+
+    // get statusCode of response
+    int statusCode = httpExecuteResponse.httpResponse().statusCode();
+
+    return Response.status(statusCode)
+        .type(MediaType.APPLICATION_JSON)
+        .entity(requestBody)
+        .header("Access-Control-Allow-Origin", "*")
+        .build();
+  }
 }
